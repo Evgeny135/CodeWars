b13d4cbaab930075d82aa08141e5130fae468100
Evgeny135 <75969458+Evgeny135@users.noreply.github.com>
2022-01-05 13:26:19 +0300

    update project

---
+++ src/main/java/NumbersInOrder.java
@@ -0,0 +1,11 @@
+public class NumbersInOrder {
+    public static boolean isAscOrder(int[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
