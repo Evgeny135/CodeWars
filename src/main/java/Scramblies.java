ad322e7b6548cd502acfae6ed30df8031eb0e293
Evgeny135 <75969458+Evgeny135@users.noreply.github.com>
2022-01-22 21:08:56 +0300

    add task "BracesChecker"

--- src/main/java/Scramblies.java
+++ src/main/java/Scramblies.java
@@ -1,11 +1,10 @@
 public class Scramblies {
     public static boolean scramble(String str1, String str2) {
-        for (int i = 0; i < str2.length(); i++) {
-            String str = String.valueOf(str2.charAt(i));
-            if (!str1.contains(str)) {
-                return false;
-            }
-            str1 = str1.replaceFirst(str, "");
+        if (str2.length() > str1.length()) return false;
+        for (String s :
+                str2.split("")) {
+            if (!str1.contains(s)) return false;
+            str1 = str1.replaceFirst(s,"");
         }
         return true;
     }
